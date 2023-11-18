ff358bef0ee6e23f95adbaaffb5f40c0de7b9f28
Aiswarya-A-M <aiswarya.am@neoito.com>
2023-11-18 20:50:30 +0530

    initial commit

---
+++ classValidator.js
@@ -0,0 +1,33 @@
+import {
+    validate,
+    validateOrReject,
+    Contains,
+    IsInt,
+    Length,
+    IsEmail,
+    IsFQDN,
+    IsDate,
+    Min,
+    Max,
+  } from 'class-validator';
+  export class Post {
+    @Length(10, 20)
+    userName: string;
+  
+    @Contains('hello')
+    text: string;
+  
+    @IsInt()
+    @Min(0)
+    @Max(10)
+    rating: number;
+  
+    @IsEmail()
+    email: string;
+  
+    @IsFQDN()
+    site: string;
+  
+    @IsDate()
+    createDate: Date;
+  }
